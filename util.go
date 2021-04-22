8f073660b93dad38d78bdcc2e47d2c01bfdd8cb0
lorenzoB07 <82948587+lorenzoB07@users.noreply.github.com>
2021-04-22 15:35:43 +0200

    GetPrimaryKeyValue

--- util.go
+++ util.go
@@ -19,20 +19,10 @@ func interfaceToString(v interface{}) string {
 //restituisce la struct con solo i campi della primary key valorizzati
 func GetPrimaryKeyValue(db *gorm.DB) interface{} {
 	if keys := db.Statement.Schema.PrimaryFields; keys != nil {
-		model := reflect.ValueOf(db.Statement.Model)
-		t := reflect.TypeOf(model)
-		primary := reflect.New(t)
-		// var keysValue string
+		model := reflect.Indirect(reflect.ValueOf(db.Statement.Model))
+		primary := reflect.New(model.Type()).Elem()
 		for _, k := range keys {
-			// v := reflect.Indirect(reflect.ValueOf(k))
-			// if v.Kind() == reflect.String || v.Kind() == reflect.Int {
 			primary.FieldByName(k.Name).Set(model.FieldByName(k.Name))
-			// } else {
-			// 	keysValue
-			// }
-
-			// var a = db.Statement.Model[k.Name]
-			// println(a)
 		}
 		return primary.Interface()
 	}
