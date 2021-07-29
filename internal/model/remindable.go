108d9933a788049717361c761a83375bbd0c3b3c
simonerocchi <s.rocchi@mind-informatica.com>
2021-07-29 15:40:17 +0200

    corretto remindable

--- internal/model/remindable.go
+++ internal/model/remindable.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Remindable interface {
 	// deve restituire slice dei reminder da inserire, slice dei reminder da cancellare e l'eventualem errore
-	Reminders(*gorm.DB, string) ([]Reminder, []Reminder, error)
+	Reminders(*gorm.DB, string) ([]Reminder, []Reminder, string, error)
 	//viene chiamato da gorm dopo insert
 	AfterCreate(*gorm.DB) error
 	//viene chiamato da gorm dopo update
